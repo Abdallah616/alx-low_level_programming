b09fe8f5229dc56f6220ef9805ddd97baaeff63a
Abdallah616 <yassinabdou59@gmail.com>
2023-08-11 05:18:03 -0700

    task no 13

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -12,19 +12,19 @@ int main(void)
 {
 	int digit1 = 0, digit2;
 
-	while (digit1 >= 99)
+	while (digit1 <= 99)
 	{
 		digit2 = digit1;
 		while (digit2 <= 99)
 		{
-			if (digit2 != digit1)
+			if (digit2 > digit1)
 			{
 				putchar((digit1 / 10) + 48);
 				putchar((digit1 % 10) + 48);
 				putchar(' ');
 				putchar((digit2 / 10) + 48);
 				putchar((digit2 % 10) + 48);
-				if (digit1 != 98 && digit2 != 99)
+				if (digit1 != 98)
 				{
 				putchar(',');
 				putchar(' ');
