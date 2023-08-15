770fc21464cc43cce5aec21d6201c97b8dbeff10
Abdallah616 <yassinabdou59@gmail.com>
2023-08-15 03:30:38 -0700

    task resolved

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,12 +5,12 @@
 */
 void print_alphabet_x10(void)
 {
-	int ch;
+	int c, i;
 
-	for (int i = 1; i <= 10; i++)
+	for (i = 1; i <= 10; i++)
 	{
 		for (ch = 'a'; ch <= 'z'; ch++)
-		_putchar(ch);
-	_putchar('\n');
+			_putchar(ch);
+		_putchar('\n');
 	}
 }
