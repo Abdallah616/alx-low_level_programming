1186468400a852476afb1298fabb6c0b84e06464
Abdallah616 <yassinabdou59@gmail.com>
2023-08-21 05:24:54 -0700

    task solved

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_rev - prints a string in reversed order
+ *
+ * @s: string input to be printed
+ *
+ * Return: Nothing
+*/
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (i < '\0')
+		i++;
+
+	while (i >= 0)
+	{
+		_putchar(s[i]);
+		i--;
+	}
+	_putchar('\n');
+}
