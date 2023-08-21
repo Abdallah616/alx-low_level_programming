809109581076492f6d157912711d3e971145a7f6
Abdallah616 <yassinabdou59@gmail.com>
2023-08-21 08:07:00 -0700

    task solved

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -12,10 +12,8 @@ void puts2(char *str)
 {
 	int i;
 
-	for (i = 0; str[i] != '\0'; i++)
-	{
-		if (s[i] % 2 == 0)
-			_putchar(str[i]);
-	}
+	for (i = 0; str[i] != '\0'; i += 2)
+		_putchar(str[i]);
+
 	_putchar('\n');
 }
