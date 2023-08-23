60d0e08b20a21be833849b3c0079983b5cec4f61
Abdallah616 <yassinabdou59@gmail.com>
2023-08-23 08:43:49 -0700

    task solved

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,14 +1,57 @@
 #include "main.h"
+#include <stdio.h>
+/**
+ * isLower - determine if character is lowercase
+ *
+ * @: character
+ *
+ * Return: (num of char in ascii) if true and 0 if false
+*/
+int isLower(char c)
+{
+	return (c >= 97 && c <= 122);
+}
+/**
+ * isdelimiter - determine if charcter is a delimiter
+ *
+ * @c: character
+ *
+ * Return: 1 if true 0 if false
+*/
+int isdelimiter(char c)
+{
+	int i;
+	char deli[] = " \t\n,;.!?\"(){}";
+
+	for (i = 0; i < 12; i++)
+	{
+		if (c == deli[i])
+			return (1);
+	}
+	return (0);
+}
 /**
  * cap_string - capitalizes all words of a string.
  *
- * @str: input string
+ * @s: input string
  *
- * Return: @str
+ * Return: @s
 */
 char *cap_string(char *s)
 {
-	int i = 0, j;
+	int foundDelimiter;
 
-	
+	while (*s)
+	{
+		if (isdelimiter(*s))
+			foundDelimiter = 1;
+		else if (isLower(*s) && foundDelimiter)
+		{
+			*s -= 32;
+			foundDelimiter = 0;
+		} else
+			foundDelimiter = 0;
+		s++;
+	}
+	return (s);
 }
