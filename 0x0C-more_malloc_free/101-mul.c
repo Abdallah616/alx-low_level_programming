4bdaa6e32789f95a14ee444b2db16047e127f46d
Abdallah616 <yassinabdou59@gmail.com>
2023-09-07 07:27:49 -0700

    task solved

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,83 +1,114 @@
 #include "main.h"
+#include <stdio.h>
+#include <string.h>
+
 /**
- * _puts - prints a string
+ * _isdigit - checks if character is digit
+ * @c: the character to check
  *
- * @str: string to be printed
+ * Return: 1 if digit, 0 otherwise
+ */
+int _isdigit(int c)
+{
+	return (c >= '0' && c <= '9');
+}
+
+/**
+ * _strlen - returns the length of a string
+ * @s: the string whose length to check
  *
- * Return: Nothing
-*/
-void _puts(char *str)
+ * Return: integer length of string
+ */
+int _strlen(char *s)
 {
 	int i = 0;
 
-	while (str[i])
-	{
-		_putchar(str[i]);
+	while (*s++)
 		i++;
-	}
+	return (i);
 }
-/**
- * _atoi - convert a string to an integer.
- *
- * @s: input string
- *
- * Return: converted int from string
-*/
-int _atoi(const char *s)
-{
-	unsigned long int num = 0, i, fnum;
-	int sign = 1;
 
-	for (fnum = 0; (s[fnum] > 57 && s[fnum] < 48); fnum++)
-	{
-		if (s[fnum] == '-')
-			sign *= -1;
-	}
-	for (i = fnum; s[i] >= 48 && s[i] <= 57; i++)
-	{
-		num *= 10;
-		num += (s[i] - 48);
-	}
-	return (sign * num);
-}
 /**
- * print_int - prints an integer
+ * big_multiply - multiply two big number strings
+ * @s1: the first big number string
+ * @s2: the second big number string
  *
- * @n: int
- *
- * Return: Nothing
-*/
-void print_int(unsigned long int n)
+ * Return: the product big number string
+ */
+char *big_multiply(char *s1, char *s2)
 {
-	unsigned long int div = 1, i, res;
+	char *r;
+	int l1, l2, a, b, c, x;
 
-	for (i = 0; n / div > 9; i++, div *= 10)
-		;
-	for (; div >= 1; n %= div, div /= 10)
+	l1 = _strlen(s1);
+	l2 = _strlen(s2);
+	r = malloc(a = x = l1 + l2);
+	if (!r)
+		printf("Error\n"), exit(98);
+	while (a--)
+		r[a] = 0;
+
+	for (l1--; l1 >= 0; l1--)
 	{
-		res = n / div;
-		_putchar('0' + res);
+		if (!_isdigit(s1[l1]))
+		{
+			free(r);
+			printf("Error\n"), exit(98);
+		}
+		a = s1[l1] - '0';
+		c = 0;
+
+		for (l2 = _strlen(s2) - 1; l2 >= 0; l2--)
+		{
+			if (!_isdigit(s2[l2]))
+			{
+				free(r);
+				printf("Error\n"), exit(98);
+			}
+			b = s2[l2] - '0';
+
+			c += r[l1 + l2 + 1] + (a * b);
+			r[l1 + l2 + 1] = c % 10;
+
+			c /= 10;
+		}
+		if (c)
+			r[l1 + l2 + 1] += c;
 	}
+	return (r);
 }
+
+
 /**
- * main - program that multiplies two positive numbers and
- *		print the result.
+ * main - multiply two big number strings
+ * @argc: the number of arguments
+ * @argv: the argument vector
  *
- * @argc: num of arguments passed
- * @argv: list of arguments
- *
- * Return: 0
-*/
-int main(int argc, char const *argv[])
+ * Return: Always 0 on success.
+ */
+int main(int argc, char **argv)
 {
-	(void)argc;
+	char *r;
+	int a, c, x;
 
 	if (argc != 3)
+		printf("Error\n"), exit(98);
+
+	x = _strlen(argv[1]) + _strlen(argv[2]);
+	r = big_multiply(argv[1], argv[2]);
+	c = 0;
+	a = 0;
+	while (c < x)
 	{
-		_puts("Error ");
-		exit(98);
+		if (r[c])
+			a = 1;
+		if (a)
+			_putchar(r[c] + '0');
+		c++;
 	}
-	print_int(_atoi(argv[1]) * _atoi(argv[2]));
+	if (!a)
+		_putchar('0');
 	_putchar('\n');
+	free(r);
 	return (0);
 }
---
+++ 0x0C-more_malloc_free/mul.c
@@ -0,0 +1,83 @@
+#include "main.h"
+/**
+ * _puts - prints a string
+ *
+ * @str: string to be printed
+ *
+ * Return: Nothing
+*/
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (str[i])
+	{
+		_putchar(str[i]);
+		i++;
+	}
+}
+/**
+ * _atoi - convert a string to an integer.
+ *
+ * @s: input string
+ *
+ * Return: converted int from string
+*/
+int _atoi(const char *s)
+{
+	unsigned long int num = 0, i, fnum;
+	int sign = 1;
+
+	for (fnum = 0; (s[fnum] > 57 && s[fnum] < 48); fnum++)
+	{
+		if (s[fnum] == '-')
+			sign *= -1;
+	}
+	for (i = fnum; s[i] >= 48 && s[i] <= 57; i++)
+	{
+		num *= 10;
+		num += (s[i] - 48);
+	}
+	return (sign * num);
+}
+/**
+ * print_int - prints an integer
+ *
+ * @n: int
+ *
+ * Return: Nothing
+*/
+void print_int(unsigned long int n)
+{
+	unsigned long int div = 1, i, res;
+
+	for (i = 0; n / div > 9; i++, div *= 10)
+		;
+	for (; div >= 1; n %= div, div /= 10)
+	{
+		res = n / div;
+		_putchar('0' + res);
+	}
+}
+/**
+ * main - program that multiplies two positive numbers and
+ *		print the result.
+ *
+ * @argc: num of arguments passed
+ * @argv: list of arguments
+ *
+ * Return: 0
+*/
+int main(int argc, char const *argv[])
+{
+	(void)argc;
+
+	if (argc != 3)
+	{
+		_puts("Error ");
+		exit(98);
+	}
+	print_int(_atoi(argv[1]) * _atoi(argv[2]));
+	_putchar('\n');
+	return (0);
+}
