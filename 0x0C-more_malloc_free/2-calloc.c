28a4435a84f5e1a86414f9a1d8ac2ff25f48ef1f
Abdallah616 <yassinabdou59@gmail.com>
2023-09-07 03:28:29 -0700

    task solved

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -14,7 +14,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	if (size == 0 || nmemb == 0)
 		return (NULL);
-	m = malloc(size * nmemb);
+	s = malloc(size * nmemb);
 	if (s == NULL)
 		return (NULL);
 	for (i = 0; i < (nmemb * size); i++)
