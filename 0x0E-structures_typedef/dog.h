095f8581ce15fc9a7dce98681331d47acfc286fd
Abdallah616 <yassinabdou59@gmail.com>
2023-09-11 06:18:53 -0700

    task solved

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a new struct named dog
+ *
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ *
+ * Description: just alone dog
+*/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
