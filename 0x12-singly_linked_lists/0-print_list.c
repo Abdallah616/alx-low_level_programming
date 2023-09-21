4b657382004ca147b3a71973591d477eb54e5bc1
Abdallah616 <yassinabdou59@gmail.com>
2023-09-21 05:56:49 -0700

    task solved

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,15 +10,22 @@ int _strlen(char *s)
 	int i = 0;
 
 	if (!s)
-		return(0);
+		return (0);
 	while (*s++)
 		i++;
 	return (i);
 }
+/**
+ * print_list - function that prints all the elements of a list_t list.
+ *
+ * @h: pointer to first node.
+ *
+ * Return: size of list.
+*/
 size_t print_list(const list_t *h)
 {
 	size_t i = 0;
-	
+
 	while (h)
 	{
 		printf("[%d] %s\n", _strlen(h->str), h->str ? h->str : "(nil)");
