a69e0b1f89ea95eb3875b24d0584e3749c8d9535
Abdallah616 <yassinabdou59@gmail.com>
2023-10-02 11:17:35 -0700

    task solved

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,5 +1,21 @@
 #include "main.h"
 /**
+ * _strlen - returns the length of a string
+ * @s: the string whose length to check
+ *
+ * Return: integer length of string
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	if (!s)
+		return (0);
+	while (*s++)
+		i++;
+	return (i);
+}
+/**
  * create_file - creates a file
  * @filename: name of file to create
  * @text_content: text to write
