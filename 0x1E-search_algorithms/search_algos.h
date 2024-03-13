833b80e75616ad7c8d9d2ed925ba9a6fb80f2f06
Abdallah616 <yassinabdou59@gmail.com>
2024-03-13 07:29:45 -0700

    task solved

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,11 @@
+#ifndef SEARCH_ALGOS_H
+#define SEARCH_ALGOS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+void print_array(int *array, int l, int r);
+int binary_search(int *array, size_t size, int value);
+int linear_search(int *array, size_t size, int value);
+
+
+#endif
